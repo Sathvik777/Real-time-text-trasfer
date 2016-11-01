2e4003197892a7eb9001e885df778f524f0aba8a
Sathvik777 <katam.sathvik@gmail.com>
2016-11-01 10:34:24 +0100

    UI added

--- real-timetexttrasfer/app/src/main/java/com/example/sathvikkatam/real_timetexttrasfer/MainActivity.java
+++ real-timetexttrasfer/app/src/main/java/com/example/sathvikkatam/real_timetexttrasfer/MainActivity.java
@@ -1,13 +1,88 @@
 package com.example.sathvikkatam.real_timetexttrasfer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends ActionBarActivity
+        implements LoaderManager.LoaderCallbacks<Cursor>{
+    private CursorAdapter cursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        insertNote("New note");
+
+        Cursor cursor = getContentResolver().query(NotesProvider.CONTENT_URI, DBConnect.ALL_COLUMNS, null, null, null, null);
+        String [] from = {DBConnect.NOTE_TEXT};
+        int[] to = {android.R.id.text1};
+
+        cursorAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_expandable_list_item_1, cursor, from , to, 0);
+        ListView list = (ListView) findViewById(android.R.id.list);
+        list.setAdapter(cursorAdapter);
+
+
+
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBConnect.NOTE_TEXT, noteText);
+        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI,
+                values);
+        Log.d("MainActivity", "Inserted note " + noteUri.getLastPathSegment());
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
