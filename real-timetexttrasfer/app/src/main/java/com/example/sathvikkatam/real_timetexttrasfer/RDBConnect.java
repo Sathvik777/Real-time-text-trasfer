d4984394e2beec5e95e715728c017843e2d339eb
Sathvik777 <katam.sathvik@gmail.com>
2016-11-01 09:50:56 +0100

    DB-Connected

---
+++ real-timetexttrasfer/app/src/main/java/com/example/sathvikkatam/real_timetexttrasfer/RDBConnect.java
@@ -0,0 +1,15 @@
+package com.example.sathvikkatam.real_timetexttrasfer;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by sathvikkatam on 11/1/16.
+ */
+
+public class RDBConnect extends AsyncTask<Void, Void, Void> {
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+}
